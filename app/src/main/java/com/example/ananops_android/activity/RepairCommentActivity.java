617265edb81f3261959429ecd93886735d5e0240
wangying929123925 <929123925@qq.com>
2019-12-02 15:08:10 +0800

    add commemt

---
+++ app/src/main/java/com/example/ananops_android/activity/RepairCommentActivity.java
@@ -0,0 +1,109 @@
+package com.example.ananops_android.activity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ananops_android.R;
+
+public class RepairCommentActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView title;
+    private ImageView back_img;
+    private Button repair_commit_button;
+    private TextView comment_repair_name;
+    private RatingBar general_comment_rat;
+    private RatingBar attitude_comment_rat;
+    private RatingBar quality_comment_rat;
+    private EditText et_comment;
+    private String comment_content;
+    private float general_comment_mum;
+    private float attitude_comment_mum;
+    private float quality_comment_mum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repair_comment);
+        initView();
+        setOnListener();
+    }
+
+    private void initView(){
+        title=findViewById(R.id.txt_title);//标题
+        back_img=findViewById(R.id.img_back);
+        repair_commit_button=findViewById(R.id.comment_submit_button);
+        comment_repair_name=findViewById(R.id.comment_repair_name);
+        general_comment_rat=findViewById(R.id.general_comment_rat);
+        attitude_comment_rat=findViewById(R.id.attitude_comment_rat);
+        quality_comment_rat=findViewById(R.id.quality_comment_rat);
+        et_comment=findViewById(R.id.et_comment);
+    }
+    private void setOnListener() {
+
+        back_img.setOnClickListener(this);
+        repair_commit_button.setOnClickListener(this);
+        general_comment_rat.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(RepairCommentActivity.this,"总体评分="+rating,Toast.LENGTH_LONG).show();
+                general_comment_mum=rating;
+            }
+        });
+        attitude_comment_rat.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(RepairCommentActivity.this,"态度评分="+rating,Toast.LENGTH_LONG).show();
+                attitude_comment_mum=rating;
+            }
+        });
+        quality_comment_rat.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(RepairCommentActivity.this,"质量评分="+rating,Toast.LENGTH_LONG).show();
+                quality_comment_mum=rating;
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+     switch (v.getId()){
+     case R.id.img_back:
+         showExitAlertDialog(v);
+         break;
+     case R.id.comment_submit_button:
+      Toast.makeText(RepairCommentActivity.this,"提交评论！",Toast.LENGTH_LONG).show();
+      break;
+     }
+    }
+    public void showExitAlertDialog(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("评价未提交，退出将不报错编辑，确认退出吗？");
+        builder.setTitle("提示");
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.create().show();
+    }
+}
--- app/src/main/java/com/example/ananops_android/util/IntentUtils.java
+++ app/src/main/java/com/example/ananops_android/util/IntentUtils.java
@@ -1,11 +1,74 @@
 package com.example.ananops_android.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 public class IntentUtils {
+    public static IntentUtils instence;
+    public static IntentUtils getInstence() {
+        if (null == instence) {
+            instence = new IntentUtils();
+        }
+        return instence;
+    }
 
-    public void actionStart(Context context, String data1, String data2, Class<?> cs){
+    private IntentUtils() {
+
+    }
+    /**
+     * 不带参数的跳转
+     *
+     * @param fromContext
+     * @param cls
+     *            泛型
+     */
+    public void intent(Context fromContext, Class<?> cls) {
+        Intent intent = new Intent(fromContext, cls);
+        fromContext.startActivity(intent);
+    }
+    /**
+     * 带参数的跳转
+     *
+     * @param fromContext
+     * @param cls
+     *            泛型
+     */
+    public void intent(Context fromContext, Class<?> cls,Bundle bb) {
+        Intent intent = new Intent(fromContext, cls);
+        intent.putExtras(bb);
+        fromContext.startActivity(intent);
+    }
+    /**
+     * 封装 startActivityForResult 带参数传�?
+     *
+     * @param fromClass
+     * @param toClass
+     * @param requestCode
+     * @param data
+     *            参数传�?
+     */
+    public void startActivityForResult(Activity fromClass, Class<?> toClass, int requestCode, Bundle data) {
+        Intent intent = new Intent();
+        intent.setClass(fromClass, toClass);
+        if (null != data) {
+            intent.putExtras(data);
+        }
+        fromClass.startActivityForResult(intent, requestCode);
+    }
+
+    /**
+     * 接收参数然后在返回数值
+     * @param fromContext 当前的activity
+     * @param bb
+     * @param RESULT_OK
+     */
+    public void setResult(Activity fromContext,Bundle bb,int RESULT_OK){
         Intent intent=new Intent();
+        intent.putExtras(bb);
+        fromContext.setResult(RESULT_OK, intent);
+        fromContext.finish();
     }
 }
+
