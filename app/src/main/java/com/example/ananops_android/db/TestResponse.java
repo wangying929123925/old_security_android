1c5505cca208c0ad6603481c8f7ded7ef6f561fd
xiaoyangCui <1227687610@qq.com>
2020-01-07 10:22:55 +0800

    complete InspectionDetailActivity with virtual data

---
+++ app/src/main/java/com/example/ananops_android/db/TestResponse.java
@@ -0,0 +1,304 @@
+package com.example.ananops_android.db;
+
+/**
+ * @author
+ * @date 2020/1/6.
+ * GitHub：Cuixiaoyang123
+ * email：1227687610@qq.com
+ * description：
+ */
+public class TestResponse {
+
+    /**
+     * code : 0
+     * message : string
+     * result : {"actualFinishTime":"2020-01-06T10:41:12.475Z","createdTime":"2020-01-06T10:41:12.475Z","creator":"string","creatorId":0,"days":0,"facilitatorId":0,"frequency":0,"id":0,"inspectionType":0,"lastOperator":"string","lastOperatorId":0,"location":"string","maintenanceCost":0,"orderBy":"string","pageNum":0,"pageSize":0,"principalId":0,"projectId":0,"remark":"string","scheduledStartTime":"2020-01-06T10:41:12.475Z","status":0,"taskName":"string","totalCost":0,"updateTime":"2020-01-06T10:41:12.475Z","version":0}
+     */
+
+    //使用虚假数据，用Retrofit时，将不用虚拟赋值
+    private long code = 45478641;
+    private String message = "国税局二楼办公室";
+    private ResultBean result = new ResultBean();
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ResultBean getResult() {
+        return result;
+    }
+
+    public void setResult(ResultBean result) {
+        this.result = result;
+    }
+
+    public static class ResultBean {
+        /**
+         * actualFinishTime : 2020-01-06T10:41:12.475Z
+         * createdTime : 2020-01-06T10:41:12.475Z
+         * creator : string
+         * creatorId : 0
+         * days : 0
+         * facilitatorId : 0
+         * frequency : 0
+         * id : 0
+         * inspectionType : 0
+         * lastOperator : string
+         * lastOperatorId : 0
+         * location : string
+         * maintenanceCost : 0
+         * orderBy : string
+         * pageNum : 0
+         * pageSize : 0
+         * principalId : 0
+         * projectId : 0
+         * remark : string
+         * scheduledStartTime : 2020-01-06T10:41:12.475Z
+         * status : 0
+         * taskName : string
+         * totalCost : 0
+         * updateTime : 2020-01-06T10:41:12.475Z
+         * version : 0
+         */
+
+        //使用虚假数据，用Retrofit时，将不用虚拟赋值
+        private String actualFinishTime = "2020-01-06T10:41:12.475Z";
+        private String createdTime = "2020-01-06T10:41:12.475Z";
+        private String creator = "邢家营";
+        private long creatorId = 0;
+        private int days = 0;
+        private long facilitatorId = 0;
+        private int frequency = 0;
+        private long id = 0;
+        private int inspectionType = 0;
+        private String lastOperator = "string";
+        private long lastOperatorId = 0;
+        private String location = "string";
+        private int maintenanceCost = 0;
+        private String orderBy = "string";
+        private int pageNum = 8232223;
+        private int pageSize = 0;
+        private long principalId = 0;
+        private long projectId = 0;
+        private String remark = "string";
+        private String scheduledStartTime = "2020-01-06T10:41:12.475Z";
+        private int status = 0;
+        private String taskName = "string";
+        private int totalCost = 0;
+        private String updateTime = "2020-01-06T10:41:12.475Z";
+        private int version = 0;
+
+        public String getActualFinishTime() {
+            return actualFinishTime;
+        }
+
+        public void setActualFinishTime(String actualFinishTime) {
+            this.actualFinishTime = actualFinishTime;
+        }
+
+        public String getCreatedTime() {
+            return createdTime;
+        }
+
+        public void setCreatedTime(String createdTime) {
+            this.createdTime = createdTime;
+        }
+
+        public String getCreator() {
+            return creator;
+        }
+
+        public void setCreator(String creator) {
+            this.creator = creator;
+        }
+
+        public long getCreatorId() {
+            return creatorId;
+        }
+
+        public void setCreatorId(long creatorId) {
+            this.creatorId = creatorId;
+        }
+
+        public int getDays() {
+            return days;
+        }
+
+        public void setDays(int days) {
+            this.days = days;
+        }
+
+        public long getFacilitatorId() {
+            return facilitatorId;
+        }
+
+        public void setFacilitatorId(long facilitatorId) {
+            this.facilitatorId = facilitatorId;
+        }
+
+        public int getFrequency() {
+            return frequency;
+        }
+
+        public void setFrequency(int frequency) {
+            this.frequency = frequency;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public int getInspectionType() {
+            return inspectionType;
+        }
+
+        public void setInspectionType(int inspectionType) {
+            this.inspectionType = inspectionType;
+        }
+
+        public String getLastOperator() {
+            return lastOperator;
+        }
+
+        public void setLastOperator(String lastOperator) {
+            this.lastOperator = lastOperator;
+        }
+
+        public long getLastOperatorId() {
+            return lastOperatorId;
+        }
+
+        public void setLastOperatorId(long lastOperatorId) {
+            this.lastOperatorId = lastOperatorId;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        public int getMaintenanceCost() {
+            return maintenanceCost;
+        }
+
+        public void setMaintenanceCost(int maintenanceCost) {
+            this.maintenanceCost = maintenanceCost;
+        }
+
+        public String getOrderBy() {
+            return orderBy;
+        }
+
+        public void setOrderBy(String orderBy) {
+            this.orderBy = orderBy;
+        }
+
+        public int getPageNum() {
+            return pageNum;
+        }
+
+        public void setPageNum(int pageNum) {
+            this.pageNum = pageNum;
+        }
+
+        public int getPageSize() {
+            return pageSize;
+        }
+
+        public void setPageSize(int pageSize) {
+            this.pageSize = pageSize;
+        }
+
+        public long getPrincipalId() {
+            return principalId;
+        }
+
+        public void setPrincipalId(long principalId) {
+            this.principalId = principalId;
+        }
+
+        public long getProjectId() {
+            return projectId;
+        }
+
+        public void setProjectId(long projectId) {
+            this.projectId = projectId;
+        }
+
+        public String getRemark() {
+            return remark;
+        }
+
+        public void setRemark(String remark) {
+            this.remark = remark;
+        }
+
+        public String getScheduledStartTime() {
+            return scheduledStartTime;
+        }
+
+        public void setScheduledStartTime(String scheduledStartTime) {
+            this.scheduledStartTime = scheduledStartTime;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+        public String getTaskName() {
+            return taskName;
+        }
+
+        public void setTaskName(String taskName) {
+            this.taskName = taskName;
+        }
+
+        public int getTotalCost() {
+            return totalCost;
+        }
+
+        public void setTotalCost(int totalCost) {
+            this.totalCost = totalCost;
+        }
+
+        public String getUpdateTime() {
+            return updateTime;
+        }
+
+        public void setUpdateTime(String updateTime) {
+            this.updateTime = updateTime;
+        }
+
+        public int getVersion() {
+            return version;
+        }
+
+        public void setVersion(int version) {
+            this.version = version;
+        }
+    }
+}
+
--- app/src/main/java/com/example/ananops_android/fragment/InspectionFragment.java
+++ app/src/main/java/com/example/ananops_android/fragment/InspectionFragment.java
@@ -10,9 +10,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ananops_android.R;
 
@@ -21,14 +23,16 @@ import java.util.List;
 
 public class InspectionFragment extends Fragment {
     private RecyclerView myRV;
-    private List<String> strings = new ArrayList<>();
+    private List<String> names = new ArrayList<>();
+    private List<String> values = new ArrayList<>();
 
     //Fragment中构造方法中不能传递参数
     //通过下面的方式 能将参数传进InspectionFragment中
-    public static InspectionFragment newIntance(ArrayList<String> strings) {
+    public static InspectionFragment newIntance(ArrayList<String> names,ArrayList<String> values) {
         InspectionFragment inspectionFragment = new InspectionFragment();
         Bundle bundle = new Bundle();
-        bundle.putStringArrayList("data", strings);
+        bundle.putStringArrayList("data", names);
+        bundle.putStringArrayList("value", values);
         inspectionFragment.setArguments(bundle);
         return inspectionFragment;
     }
@@ -38,50 +42,81 @@ public class InspectionFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.view_show, container, false);
         Bundle bundle = getArguments();
-        strings  = bundle.getStringArrayList("data");
+        names = bundle.getStringArrayList("data");
+        values = bundle.getStringArrayList("value");
         myRV = view.findViewById(R.id.rv_inspection);
         myRV.setLayoutManager(new LinearLayoutManager(getContext()));
-        myRV.setAdapter(new MyRecyclerViewAdapter(strings, getContext()));
+        myRV.setAdapter(new MyRecyclerViewAdapter(names, values, getContext()));
+
         return view;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-    }
 
     public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.InnerHolder> {
 
+        private List<String> mValues;
         private List<String> mData;
         private Context mContext;
 
 
         public class InnerHolder extends RecyclerView.ViewHolder {
+            private RelativeLayout mRl;
             private EditText mContent;
             private TextView mTitle;
+            private Button mButton;
 
             public InnerHolder(@NonNull View itemView) {
                 super(itemView);
+                mRl = itemView.findViewById(R.id.rl_rl);
                 mContent = itemView.findViewById(R.id.et_inspection_content);
                 mTitle = itemView.findViewById(R.id.tv_inspection_title);
+                mButton = itemView.findViewById(R.id.bt_item);
             }
         }
-        public MyRecyclerViewAdapter(List<String> mData, Context mContext) {
+
+        public MyRecyclerViewAdapter(List<String> mData, List<String> values, Context mContext) {
+            this.mValues = values;
             this.mData = mData;
             this.mContext = mContext;
         }
 
         @NonNull
         @Override
-        public InnerHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        public InnerHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, final int i) {
             View view = LayoutInflater.from(mContext).inflate(R.layout.item_sublist, null);
-            return new InnerHolder(view);
+            final InnerHolder innerHolder = new InnerHolder(view);
+
+            innerHolder.mButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mContext, "click the"+ innerHolder.mTitle.getText(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            return innerHolder;
         }
 
         @Override
-        public void onBindViewHolder(@NonNull InnerHolder innerHolder, int i) {
+        public void onBindViewHolder(@NonNull InnerHolder innerHolder, final int i) {
             innerHolder.mTitle.setText(mData.get(i));
+            innerHolder.mContent.setText(mValues.get(i));
+            innerHolder.mRl.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mContext, "点击了条目" + i, Toast.LENGTH_SHORT).show();
+                }
+            });
+            //电话可修改
+            if (mData.get(i).contains("电话")||mData.get(i).contains("等级")) {
+                innerHolder.mContent.setFocusableInTouchMode(true);
+                innerHolder.mButton.setVisibility(View.VISIBLE);
+                innerHolder.mButton.setText("可修改");
+            }
+            if (i == 2) {
+                if (mData.get(i).equals("审核人")) {
+                    innerHolder.mButton.setVisibility(View.VISIBLE);
+                }
+            }
         }
 
         @Override
